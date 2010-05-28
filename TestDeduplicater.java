24f2ca07e547b9fd6d65749d0dcff2d32807a85b
Brian Tubergen <brian.tubergen@gmail.com>
2010-05-28 10:19:32 -0400

    Another backup

--- TestDeduplicater.java
+++ TestDeduplicater.java
@@ -1,6 +1,12 @@
+import au.com.bytecode.opencsv.CSVReader;
+import au.com.bytecode.opencsv.CSVWriter;
+
+import java.io.FileReader;
+import java.io.IOException;
+
 /*************************************************************************
  *  Brian Tubergen
- *  05/27/10
+ *  05/28/10
  *
  *  Compilation:  javac TestDeduplicater.java
  *  Execution:    java org.junit.runner.JUnitCore TestDeduplicater
@@ -14,115 +20,129 @@ import org.junit.* ;
 import static org.junit.Assert.* ;
 
 public class TestDeduplicater {    
-  
-   @Test
-   public void test_rt_regex()
-   {
-       System.out.println("Testing regular expression for rt..");
 
-       Deduplicater d = new Deduplicater("test_rt.csv");
+    /* Ensure that the regular expression meant to remove RT's at the beginning
+     * of ideas functions properly by testing that all ideas marked with "keep"
+     * in the specified text file are actually kept.
+     */
+    @Test
+    public void test_rt_regex()
+    {
+        System.out.println("Testing regular expression for rt..");
 
-       d.removeRTs();
+        Deduplicater d = new Deduplicater("test_csvfiles/test_rt.csv");
 
-       //  The entries with "keep" should keep an RT that doesn't appear
-       //  at the beginning; the others should have none
-       for (String idea : d.getIdeas())
-       {
-           if (idea.contains("keep"))
-               assertTrue(idea.contains("RT"));
-           else
-               assertTrue(!idea.contains("RT"));
-       }
+        d.removeRTs();
+
+        //  The entries with "keep" should keep an RT that doesn't appear
+        //  at the beginning; the others should have none
+        for (String idea : d.getIdeas())
+        {
+            if (idea.contains("keep"))
+                assertTrue(idea.contains("RT"));
+            else
+                assertTrue(!idea.contains("RT"));
+        }
    }
 
-   @Test
-   public void test_whitehouse_regex()
-   {
-       System.out.println("Testing regular expression for whitehouse...");
+    /* Ensure that the regular expression meant to remove @whitehosue functions
+     * properly by testing that all @whitehouse is eliminated and that all but
+     * three of the ideas in the particular text file specified are removed as
+     * duplicates.
+     */
+    @Test
+    public void test_whitehouse_regex()
+    {
+        System.out.println("Testing regular expression for whitehouse...");
 
-       Deduplicater d = new Deduplicater("test_whitehouse.csv");
+        Deduplicater d = new Deduplicater("test_csvfiles/test_whitehouse.csv");
+        d.removeWhitehouses();
 
-       //  All but three ideas should be eliminated
-       d.removeWhitehouses();
-       assertTrue(d.size() == 3);
+        //  Should eliminate all @whitehouse
+        for (String idea : d.getIdeas())
+            assertTrue(!idea.contains("@whitehouse"));
 
-       //  Should eliminate all @whitehouse
-       for (String idea : d.getIdeas())
-           assertTrue(!idea.contains("@whitehouse"));
-   }
+        //  All but three ideas should be eliminated
+        assertTrue(d.size() == 3);
+    }
 
-   @Test
-   public void test_hashtag_regex()
-   {
-       System.out.println("Testing regular expression for hashtag...");
+    /* Ensure that the regular expression meant to remove #text functions
+     * properly by testing that all # is eliminated and that all but four of
+     * the ideas in the particular text file specified are removed as duplicates.
+     */
+    @Test
+    public void test_hashtag_regex()
+    {
+        System.out.println("Testing regular expression for hashtag...");
 
-       Deduplicater d = new Deduplicater("test_hashtag.csv");
+        Deduplicater d = new Deduplicater("test_csvfiles/test_hashtag.csv");
+        d.removeHashTags();
 
-       //  All but four ideas should be eliminated
-       d.removeHashTags();
-       assertTrue(d.size() == 4);
+        //  Should eliminate all #
+        for (String idea : d.getIdeas())
+            assertTrue(!idea.contains("#"));
 
-       //  Should eliminate all @
-       for (String idea : d.getIdeas())
-           assertTrue(!idea.contains("#"));
-   }
+        //  All but four ideas should be eliminated
+        assertTrue(d.size() == 4);
+    }
 
-   @Test
-   public void test_attext_regex()
-   {
-       System.out.println("Testing regular expression for @text...");
+    /* Ensure that the regular expression meant to remove @text functions
+     * properly by testing that all @ is eliminated and that all but three of
+     * the ideas in the particular text file specified are removed as duplicates.
+     */
+    @Test
+    public void test_attext_regex()
+    {
+        System.out.println("Testing regular expression for @text...");
 
-       Deduplicater d = new Deduplicater("test_attext.csv");
+        Deduplicater d = new Deduplicater("test_csvfiles/test_attext.csv");
+        d.removeAtText();
 
-       //  Should remove all but 3
-       d.removeAtText();       
-       assertTrue(d.size() == 3);
+        //  Should elimiante all @
+        for (String idea : d.getIdeas())
+            assertTrue(!idea.contains("@"));
 
-       //  Should elimiante all @
-       for (String idea : d.getIdeas())
-           assertTrue(!idea.contains("@"));
-   }
-/*
-   public void test_msalganik()
-   {
-       Deduplicater d = new Deduplicater("test_msalganik");
-   }
-*/
+        //  Should remove all but 3 ideas
+        assertTrue(d.size() == 3);
+    }
 
-   /*  Note: This test is kind of trivial since STs don't allow duplicate keys.
-    *  Nonetheless, I've left it here in case the Deduplicater implementation
-    *  is changed.
-    */
-   @Test
-   public void test_uniqueness_before_removals()
-   {
-       System.out.println("Testing uniqueness without remove methods" +
-               " applied...");
+    /*  This test is kind of trivial since STs don't allow duplicate keys.
+     *  Nonetheless, I've left it here in case the Deduplicater implementation
+     *  is changed.
+     */
+    @Test
+    public void test_uniqueness_before_removals()
+    {
+        System.out.println("Testing uniqueness without remove methods" +
+                " applied...");
 
-       Deduplicater d = new Deduplicater("test_uniq.csv");
+        Deduplicater d = new Deduplicater("test_csvfiles/test_uniq.csv");
 
-       //  Sets do not allow duplicates
-       SET<String> ideaSET = new SET<String>();
-       for (String idea : d.getIdeas())
-       {
-           assertTrue(!ideaSET.contains(idea));
-           ideaSET.add(idea);
-       }
+        //  Sets do not allow duplicates
+        SET<String> ideaSET = new SET<String>();
+        for (String idea : d.getIdeas())
+        {
+            assertTrue(!ideaSET.contains(idea));
+            ideaSET.add(idea);
+        }
 
-       //  The last idea in uniq.csv gives the number of unique ideas
-       int numUnique = Integer.parseInt(ideaSET.min());
-       assertTrue((Integer) ideaSET.size() == ideaSET.size());
-   }
+        //  The last idea in uniq.csv gives the number of unique ideas
+        int numUnique = Integer.parseInt(ideaSET.min());
+        assertTrue((Integer) ideaSET.size() == ideaSET.size());
+    }
 
-   @Test
-   public void test_commas()
-   {
+    /* Ensure that all the ideas that are entered which contain commas within
+     * the idea retain those commas at the end. (Ensure that they are not split
+     * in parsing the CSV file)
+     */
+    @Test
+    public void test_commas()
+    {
         System.out.println("Testing commas...");
 
-        Deduplicater d = new Deduplicater("test_commas.csv");
+        Deduplicater d = new Deduplicater("test_csvfiles/test_commas.csv");
 
-        //  Ensure that ideas that came with commas finish with commas by
-        //  marking the ones that started without commas; the rest must contain
+        //  Mark the ideas that started without commas; the rest must contain
         //  commas at the end
         boolean[] indicesWithoutCommas = new boolean[d.size()];
         for (Integer id : d.getIds())
@@ -134,19 +154,25 @@ public class TestDeduplicater {
         
         d.applyAllRemoves(false);
 
+        //  Check that the ideas that came with commas finish with commas
         for (Integer id : d.getIds())
         {
             String idea = d.getIdea(id);
             assertTrue(indicesWithoutCommas[id] || idea.contains(","));
         }
-   }
+    }
 
-   @Test
-   public void test_newlines()
-   {
+    /* Ensures that newlines are eliminated from idea output and that ideas
+     * that become identical after newlines are removed are deduplicated. Note
+     * that newline removal is automatic; there is no separate removeNewlines
+     * method.
+     */
+    @Test
+    public void test_newlines()
+    {
         System.out.println("Testing newlines...");
 
-        Deduplicater d = new Deduplicater("test_newlines.csv");
+        Deduplicater d = new Deduplicater("test_csvfiles/test_newlines.csv");
 
         //  Ensure that all newlines are eliminated
         for (String idea : d.getIdeas())
@@ -154,7 +180,32 @@ public class TestDeduplicater {
 
         //  Ensure that removing newlines removes duplicate ideas
         assertTrue(d.size() == 2);
+    }
 
-   }
- 
+    /* Compare outputs generated by a Deduplicator to the ideas that we know
+     * are the correct outputs for Matt Salganik's prescribed corner cases.
+     */
+    @Test
+    public void test_msalganik() throws IOException
+    {
+        System.out.println("Testing Matt's corner cases...");
+
+        String inputFile = "test_csvfiles/test_msalganik.csv";
+        String correctOutputFile = "test_csvfiles/test_msalganik_output.csv";
+
+        Deduplicater d = new Deduplicater(inputFile);
+        d.applyAllRemoves(false);
+
+        CSVReader reader = new CSVReader(new FileReader(correctOutputFile));
+
+        //  Store the correct idea outputs in a SET
+        SET<String> correctIdeas = new SET<String>();
+        String[] currentLine;
+        while ( (currentLine = reader.readNext()) != null)
+            correctIdeas.add(currentLine[2]);
+
+        //  Ensure we got the expected results from d
+        for (String idea : d.getIdeas())
+             assertTrue(correctIdeas.contains(idea));
+    }
 }
